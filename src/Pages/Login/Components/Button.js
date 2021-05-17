b0f733815730a79ce0d126a35215dc409262a3f1
taehyunKim <office.div1201@gmail.com>
2021-05-18 01:14:31 +0900

    Add: 비동기 함수를 활용한 버튼 클릭 시 화면 이동 이벤트 구현

--- src/Pages/Login/Components/Button.js
+++ src/Pages/Login/Components/Button.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import "./Button.scss";
 
 class Button extends Component {
@@ -8,15 +9,27 @@ class Button extends Component {
     return "";
   };
 
+  goToMain = (email, password) => {
+    const { regExp } = this.props;
+    if (email.match(regExp) && password.length >= 8)
+      return setTimeout(() => {
+        alert("로그인 성공!");
+        this.props.history.push("/main");
+      }, 1000);
+    return setTimeout(() => alert("올바른 작성을 부탁드려요."));
+  };
+
   render() {
     const { type } = this.props;
     const { email, password } = this.props.value;
     return (
       <div className="Button">
-        <button className={`${this.btnValidator(email, password)}`}>{type}</button>
+        <button className={`${this.btnValidator(email, password)}`} onClick={() => this.goToMain(email, password)}>
+          {type}
+        </button>
       </div>
     );
   }
 }
 
-export default Button;
+export default withRouter(Button);
